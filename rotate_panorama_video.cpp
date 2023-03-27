36d2057b41e69ea25ba267f85b1757d9ba554381
贾文权 <1173551915@qq.com>
2023-03-28 04:31:28 +0800

    w

--- rotate_panorama_video.cpp
+++ rotate_panorama_video.cpp
@@ -3,25 +3,44 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/videoio.hpp>
+#include <opencv2/opencv.hpp>
 
 // createLUT 和 rotatePanoramaWithLUT 函数
-void createLUT(cv::Mat &mapX, cv::Mat &mapY, int width, int height, double pitch, double yaw)
+void createLUT(cv::Mat &mapX, cv::Mat &mapY, int width, int height, double pitch, double yaw, double roll)
 {
-    double centerX = width / 2.0, centerY = height / 2.0;
+    double centerX = width / 2.0;
+    double centerY = height / 2.0;
+    double radius = width / (2 * M_PI);
+
+    cv::Mat rotMat = cv::Mat::eye(3, 3, CV_64F);
+    cv::Mat rotMatX, rotMatY, rotMatZ;
+
+    cv::Mat xAxis = (cv::Mat_<double>(3, 1) << 1, 0, 0);
+    cv::Mat yAxis = (cv::Mat_<double>(3, 1) << 0, 1, 0);
+    cv::Mat zAxis = (cv::Mat_<double>(3, 1) << 0, 0, 1);
+
+    cv::Rodrigues(xAxis * pitch * M_PI / 180.0, rotMatX);
+    cv::Rodrigues(yAxis * yaw * M_PI / 180.0, rotMatY);
+    cv::Rodrigues(zAxis * roll * M_PI / 180.0, rotMatZ);
+
+    rotMat = rotMatZ * rotMatY * rotMatX;
+
     for (int y = 0; y < height; y++)
     {
         for (int x = 0; x < width; x++)
         {
-            double theta = (2 * M_PI * (x + yaw)) / width - M_PI;
-            double phi = (M_PI * (y + pitch)) / height - M_PI / 2;
-            double xPos = cos(phi) * cos(theta) * centerX + centerX;
-            double yPos = cos(phi) * sin(theta) * centerY + centerY;
+            double theta = (2 * M_PI * x) / width - M_PI;
+            double phi = (M_PI * y) / height - M_PI / 2;
+            cv::Mat point3D = (cv::Mat_<double>(3, 1) << radius * cos(phi) * cos(theta), radius * sin(phi), radius * cos(phi) * sin(theta));
+            cv::Mat rotatedPoint3D = rotMat * point3D;
+
+            double xPos = (atan2(rotatedPoint3D.at<double>(0), rotatedPoint3D.at<double>(2)) + M_PI) * width / (2 * M_PI);
+            double yPos = (asin(rotatedPoint3D.at<double>(1) / radius) + M_PI / 2) * height / M_PI;
             mapX.at<float>(y, x) = static_cast<float>(xPos);
             mapY.at<float>(y, x) = static_cast<float>(yPos);
         }
     }
 }
-
 void rotatePanoramaWithLUT(const cv::Mat &input, cv::Mat &output, const cv::Mat &mapX, const cv::Mat &mapY)
 {
 
@@ -39,13 +58,13 @@ int main()
     int height = static_cast<int>(videoCapture.get(cv::CAP_PROP_FRAME_HEIGHT));
     double fps = videoCapture.get(cv::CAP_PROP_FPS);
 
-    double pitch = -90.0, yaw = 180.0;
+    double pitch = 0.0, yaw = 90.0, roll = 90.0;
     cv::Mat mapX = cv::Mat(height, width, CV_32FC1);
     cv::Mat mapY = cv::Mat(height, width, CV_32FC1);
-    createLUT(mapX, mapY, width, height, pitch, yaw);
+    createLUT(mapX, mapY, width, height, pitch, yaw, roll);
 
     std::string outputVideoPath = "/home/goslam/Documents/RS全景镜头/全景_VID_20230328_013007_out.mp4";
-    cv::VideoWriter videoWriter(outputVideoPath, cv::VideoWriter::fourcc('M', 'P', '4', 'V'), fps, cv::Size(width, height));
+    cv::VideoWriter videoWriter(outputVideoPath, cv::VideoWriter::fourcc('a', 'v', 'c', '1'), fps, cv::Size(width, height));
 
     if (!videoWriter.isOpened())
         return -1;
